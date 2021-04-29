5e20e94dd513935aec40116887680d56d4cbfe8f
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-04-30 00:32:06 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -1,10 +1,10 @@
   
 /**
  * @file activity1.c
- * @author
- * @brief User defined utilities, to define pins and ports to Blink an LED ON/OFF 
+ * @author Hemanth
+ * @brief blink an led 
  * @version 0.1
- * @date 2021-04-27
+ * @date 2021-04-28
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -12,14 +12,19 @@
 
 #include "activity1.h"
 
-
+/* Peripheral state*/
 void peripheral_init(void)
 {	
-	DDRD |= (1<<PD2); // set PD2=1 for LED
-    DDRD &= ~(1<<PD0); //clear bit
-    PORTD |= (1<<PD0); //set bit PD0 for SeatSwitch
-    DDRD &= ~(1<<PD1); //clear bit
-    PORTD |= (1<<PD1); //set bit PD0 for HeaterSwitch
+    /* PD2 high for Led	
+    DDRD |= (1<<PD2); 
+    /* Clearing the PD0
+    DDRD &= ~(1<<PD0);
+    /*Making High for seatbutton*/
+    PORTD |= (1<<PD0); 
+    /* Clearing the PD1
+    DDRD &= ~(1<<PD1); 
+    /*Making High for heater*/
+    PORTD |= (1<<PD1); 
 }
 
 void TurnLED_ON(){
@@ -34,11 +39,13 @@ int act1=0;
 int activity1_LED(void)
 {
        peripheral_init();
-        if(!(PIND&(1<<BUTTON_SENSOR )) && !(PIND&(1<<TEMP_SENSOR))) //both the switches are pressed
+	/* if both the buttons are high the led is high*/
+        if(!(PIND&(1<<BUTTON_SENSOR )) && !(PIND&(1<<TEMP_SENSOR))) 
         { 
             act1=1;
         }
-        else  //in all other cases
+	/*rest cases led is low*/
+        else 
         {
             act1=0;
         }

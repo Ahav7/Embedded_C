d547873bd8ec68c5571b29a74e7c3401dfa06b33
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-04-30 21:32:23 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -19,7 +19,7 @@ void peripheral_init(void)
     DDRD &= ~(1<<PD0); //clear bit
     PORTD |= (1<<PD0); //set bit PD0 for SeatSwitch
     DDRD &= ~(1<<PD1); //clear bit
-    PORTD |= (1<<PD1); //set bit PD0 for HeaterSwitch
+    PORTD |= (1<<PD1); //set bit PD0 for Heaterswitch
 }
 
 void TurnLED_ON(){

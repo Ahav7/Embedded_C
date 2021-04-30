efecaa988a29bd9c3a576955942c3e7cc355a2f2
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-04-30 15:58:39 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -1,5 +1,3 @@
-#ifndef __ACTIVITY_4_H__
-#define __ACTIVITY_4_H__
 /**
  * @file activity4.h
  * @author 
@@ -10,42 +8,29 @@
  * @copyright Copyright (c) 2021
  * 
  */
+#ifndef __ACTIVITY_4_H__
+#define __ACTIVITY_4_H__
 
-/**
- * Macro Definitions
- */
-#define F_CPU 16000000UL /**< Clock Frequency of MCU is 16 MHz */
-#define USART_BAUDRATE 9600 /**< Baud rate for serial communication  */
-#define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) /**< Formula to calculate UBRR value */
+#define F_CPU 16000000UL 
 
-/**
- * Include files
- */ 
+/* Baud Rate*/
+#define USART_BAUDRATE 9600 
+
+/* Formula to calculate UBRR value */
+#define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) 
+ 
 #include <util/delay.h>
 #include <avr/io.h>
 
-/**
- * Function Definitions
- */
 
-/**
- * @brief Setting Baud rate and Enabling Rx and Tx 
- * 
- */
+/* Enabling Tx,Rx*/
 void USARTInit();
 
-/**
- * @brief Reading data
- * 
- * @return int The read data
- */
+
 int USARTRead();
 
-/**
- * @brief Writing data to view in serial monitor
- * 
- * @param[in] temp The ADC value
- */
+/* Write data on serial monitor*/
+
 void activity4_USARTWrite(uint16_t temp);
 
 #endif /* __ACTIVITY_4_H__ */

c1814c2eac781c876c33e6e2069963c786837bde
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-04-30 23:29:06 +0530

    Update activity2.h

--- inc/activity2.h
+++ inc/activity2.h
@@ -11,35 +11,23 @@
  * 
  */
 
-/**
- * Include files
- */ 
 #include <util/delay.h>
 #include <avr/io.h>
 
-/**
- * Function Definitions
- */
 
 /**
  * @brief Initialize the registers for ADC setup
  * 
  */
 void InitADC();
-
 /**
- * @brief Gives the ADC values for the selected channel
+ * @brief Gives the ADC values for  selected channel
  * 
- * @param[in] ch Channel number from 0-7
- * @return uint16_t ADC value
  */
 uint16_t ReadADC(uint8_t ch);
-
 /**
  * @brief Return the ADC value to main function
  * 
- * @return uint16_t The ADC value
  */
 uint16_t activity2_GetADC(void);
-
-#endif /* __ACTIVITY_2_H__ */
+#endif

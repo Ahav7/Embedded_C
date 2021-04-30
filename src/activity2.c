db0adceb2209c1756642a739ce4a70599b989156
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-04-30 23:36:31 +0530

    Update activity2.c

--- src/activity2.c
+++ src/activity2.c
@@ -1,9 +1,8 @@
 /**
  * @file activity2.c
- * @author Hemanth
- * @brief Functions to get the ADC values 
+ * @author Hemanth 
  * @version 0.1
- * @date 2021-04-27
+ * @date 2021-04-28
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -11,9 +10,11 @@
 
 #include "activity2.h"
 
+
 void InitADC()
 {
     ADMUX=(1<<REFS0);
+    
     ADCSRA= (1<<ADEN)|(7<<ADPS0);
 }
 
@@ -24,14 +25,18 @@ uint16_t ReadADC(uint8_t ch)
     ADMUX|=ch;
 
     ADCSRA|=(1<<ADSC);
+    
     while(!(ADCSRA & (1<<ADIF)));
+    
     ADCSRA|=(1<<ADIF);
+    
     return(ADC);
 }
 
 uint16_t activity2_GetADC(void)
 {
       InitADC();
+    
       uint16_t temp;
       temp=ReadADC(0);
       return temp;

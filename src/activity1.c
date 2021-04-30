0739f120c64406c90d4fba97dab473af93cef02c
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-04-30 23:35:34 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -1,7 +1,6 @@
 /**
  * @file activity1.c
  * @author Hemanth
- * @brief User defined utilities, to define pins and ports to Blink an LED ON/OFF 
  * @version 0.1
  * @date 2021-04-27
  * 
@@ -14,11 +13,16 @@
 
 void peripheral_init(void)
 {	
-	DDRD |= (1<<PD2); // set PD2=1 for LED
-    DDRD &= ~(1<<PD0); //clear bit
-    PORTD |= (1<<PD0); //set bit PD0 for SeatSwitch
-    DDRD &= ~(1<<PD1); //clear bit
-    PORTD |= (1<<PD1); //set bit PD0 for HeaterSwitch
+	DDRD |= (1<<PD2);
+	// set PD2=1 for LED
+    DDRD &= ~(1<<PD0);
+	//clear bit
+    PORTD |= (1<<PD0);
+	//set bit PD0 for SeatSwitch
+    DDRD &= ~(1<<PD1);
+	//clear bit
+    PORTD |= (1<<PD1);
+	//set bit PD0 for HeaterSwitch
 }
 
 void TurnLED_ON(){
@@ -33,11 +37,12 @@ int act1=0;
 int activity1_LED(void)
 {
        peripheral_init();
-        if(!(PIND&(1<<BUTTON_SENSOR )) && !(PIND&(1<<TEMP_SENSOR))) //both the switches are pressed
+	// both inputs are high
+        if(!(PIND&(1<<BUTTON_SENSOR )) && !(PIND&(1<<TEMP_SENSOR))) 
         { 
             act1=1;
         }
-        else  //in all other cases
+        else  
         {
             act1=0;
         }

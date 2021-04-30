19f477d823bf752952d192a364194a20cf41ab8f
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-04-30 21:46:14 +0530

    Update activity1.h

--- inc/activity1.h
+++ inc/activity1.h
@@ -10,7 +10,6 @@
  */
 #ifndef __ACTIVITY_1_H__
 #define __ACTIVITY_1_H__
-
 #define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
 #define LED_PORT (PORTD)    /**< LED Port Number */
 #define LED_PIN  (PORTD2)   /**< LED Pin number  */

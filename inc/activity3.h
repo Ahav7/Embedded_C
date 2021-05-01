ee0d7450ace78aef661e9d6e7c6e25e0efa893ca
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-05-01 14:03:13 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -5,15 +5,13 @@
  * @author Hemanth
  * @brief Header file for activity3
  * @version 0.1
- * @date 2021-04-27
+ * @date 2021-04-28
  * 
  * @copyright Copyright (c) 2021
  * 
  */
 
-/**
- * Macro Definitions
- */
+
 // PWM 20%
 #define PWM_20_PERCENT (205) 
 

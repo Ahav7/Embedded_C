a8a45a814f4469af66a67eab173ea4f8311cce30
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-05-01 14:03:56 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -5,16 +5,19 @@
  * @author Hemanth
  * @brief Header file for activity4
  * @version 0.1
- * @date 2021-04-27
+ * @date 2021-04-29
  * 
  * @copyright Copyright (c) 2021
  * 
  */
 
 #define F_CPU 16000000UL 
+
 // BAud Rate
 #define USART_BAUDRATE 9600 
-/**< Formula to calculate UBRR value */
+
+
+/* UBRR value */
 #define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) 
  
 #include <util/delay.h>
